f83123b0c3dbcf3e96bb02dc7fecd1df29c1f170
Dai Nguyendo <dainguyendo@gmail.com>
2016-08-28 22:11:55 -0400

    Organized js files and renamed title

--- js/app.js
+++ js/app.js
@@ -18,17 +18,13 @@ require([
   "esri/widgets/BasemapToggle",
   "esri/views/ui/UI",
 
-  "esri/config",
-
   "dojo/domReady!"
 ], function(Map, MapView,
   Point, Extent, PictureMarkerSymbol, Graphic, PopupTemplate,
   FeatureLayer, SimpleFillSymbol, Color, ClassBreaksRenderer,
-  Legend, Home, BasemapToggle, UI,
-  esriConfig) {
+  Legend, Home, BasemapToggle, UI) {
 
-  //esriConfig.corsDetection = false;
-
+//Creating symbols for the class breaks
   var less35 = new SimpleFillSymbol({
         color: "#4da6ff",
         style: "solid",
@@ -61,6 +57,7 @@ require([
           color: "white"
         }
       });
+  //creating the renderer with the class break symbols for the targetted fields
       var renderer = new ClassBreaksRenderer({
         field: "HISPPOP_CY",
         normalizationField: "TOTPOP",
@@ -95,7 +92,7 @@ require([
           label: "> 75%"
         }]
       });
-
+//create the feature layer and apply the renderer
   var southernTexas = new FeatureLayer({
     url: "https://services.arcgis.com/V6ZHFr6zdgNZuVG0/ArcGIS/rest/services/pop_growth_south_texas/FeatureServer/0",
     renderer: renderer,
@@ -105,13 +102,12 @@ require([
       content: "Hispanic Population: {HISPPOP_CY} and Total Population: {TOTPOP}"
     }
   });
-
 //init the web map and the map's view
-  map = new Map({
+  var map = new Map({
     basemap: "streets",
     layers: [southernTexas]
   });
-  mainView = new MapView({
+  var mainView = new MapView({
     container: "map",
     map: map,
     zoom: 8,
@@ -119,15 +115,13 @@ require([
     popup: {
           dockEnabled: true,
           dockOptions: {
-            // Disables the dock button from the popup
             buttonEnabled: false,
-            // Ignore the default sizes that trigger responsive docking
             breakpoint: false,
             position: "bottom-center"
           }
         }
   });
-
+//creating legend widget
   var texasLegend = new Legend({
     view: mainView,
     layerInfos: [{
@@ -135,16 +129,11 @@ require([
       title: "Hispanic Population Divided by Total Population"
     }]
   });
-mainView.ui.add(texasLegend, "bottom-left");
-
 //Toggle basemap widget
   var toggleBasemap = new BasemapToggle({
     view: mainView,
     nextBasemap: "topo"
   });
-  mainView.ui.add(toggleBasemap, "bottom-right");
-  toggleBasemap.startup();
-
 //Creating the Blue Raster Office Graphic
   var brOffice = new Point({
     latitude: 38.890849,
@@ -176,38 +165,14 @@ mainView.ui.add(texasLegend, "bottom-left");
     }),
     visible: true
   });
-
 //UI element to recenter map to washington, dc
   var dcCenter = new Home();
-  mainView.ui.add(dcCenter, "bottom-right");
-  dcCenter.startup();
-  dcCenter.on("click", goToDC);
-  function goToDC(evt){
-    mainView.goTo(
-      {
-      center: [-77.03, 38.91],
-      zoom: 11
-      },
-      {
-        animate: true,
-        duration: 500
-      }
-    )
-  };
-
 //Add click event and shows a popup with coordinates
   mainView.on("click", addPoint);
-
-  function addPoint(evt){
-    var latitude = evt.mapPoint.getLatitude();
-    var longitude = evt.mapPoint.getLongitude();
-    mainView.popup.open({
-      title: "Coordinates",
-      content: "Latitude: " + latitude.toFixed(2) + ", Longitude: " + longitude.toFixed(2),
-      location: evt.mapPoint
-    });
-  };
-
+  dcCenter.on("click", goToDC);
+//creating event listenings that navigates map to different views
+  document.getElementById("br-logo").addEventListener("click", goToOffice);
+  document.getElementById("nav-texas").addEventListener("click", goToTexas);
 //add the blue raster office graphic when scale is close enough
   mainView.watch("viewpoint.scale", function(newScale, oldScale){
     if (newScale <= 35000) {
@@ -217,11 +182,17 @@ mainView.ui.add(texasLegend, "bottom-left");
       mainView.graphics.remove(brGraphic);
     }
   });
-
-
-  document.getElementById("br-logo").addEventListener("click", goToOffice);
-  document.getElementById("nav-texas").addEventListener("click", goToTexas);
-
+//display lat and long on click function
+  function addPoint(evt){
+    var latitude = evt.mapPoint.getLatitude();
+    var longitude = evt.mapPoint.getLongitude();
+    mainView.popup.open({
+      title: "Coordinates",
+      content: "Latitude: " + latitude.toFixed(2) + ", Longitude: " + longitude.toFixed(2),
+      location: evt.mapPoint
+    });
+  };
+//change view to southern texas
   function goToTexas(){
     mainView.goTo(
       {
@@ -232,19 +203,36 @@ mainView.ui.add(texasLegend, "bottom-left");
         duration: 500
       }
     )
-  }
-
+  };
+//change view to washington, dc
+  function goToDC(evt){
+    mainView.goTo(
+      {
+        center: [-77.03, 38.91],
+        zoom: 11
+      }, {
+        animate: true,
+        duration: 500
+      }
+    )
+  };
+//change view to blue raster office
   function goToOffice(){
     mainView.goTo(
-        {
+      {
         center: [-77.08614862698474, 38.890812860154256],
         zoom: 19
-        },
-        {
-          animate: true,
-          duration: 500
-        }
-      )
+      }, {
+        animate: true,
+        duration: 500
+      }
+    )
   };
-
+//adding elements to the map UI
+  mainView.ui.add(texasLegend, "bottom-left");
+  mainView.ui.add(toggleBasemap, "bottom-right");
+  mainView.ui.add(dcCenter, "bottom-right");
+//starting the dc centering and basemap widgets
+  dcCenter.startup();
+  toggleBasemap.startup();
 });
